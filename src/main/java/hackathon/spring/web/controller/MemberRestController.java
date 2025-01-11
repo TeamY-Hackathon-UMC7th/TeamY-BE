c664158390eb0db16c5f6033d8369b69d40e9827
lehojun <hojunchris@naver.com>
2025-01-12 02:14:10 +0900

    Member 수정

--- src/main/java/hackathon/spring/web/controller/MemberRestController.java
+++ src/main/java/hackathon/spring/web/controller/MemberRestController.java
@@ -3,6 +3,9 @@ package hackathon.spring.web.controller;
 import hackathon.spring.apiPayload.ApiResponse;
 import hackathon.spring.service.MemberService;
 import hackathon.spring.web.dto.MemberDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,19 +19,25 @@ public class MemberRestController {
 
     // 닉네임 중복 체크
     @GetMapping("/check/{nickname}")
-    public ResponseEntity<ApiResponse<String>> checkNickname(@PathVariable String nickname) {
+    public ResponseEntity<ApiResponse<String>> checkNickname(@PathVariable
+                                                                 @NotNull @NotBlank
+                                                                 @Size(max=20) String nickname) {
         return memberService.checkNickname(nickname);
     }
 
     // 회원가입
     @PostMapping("/signup")
-    public ResponseEntity<ApiResponse<String>> signUp(@RequestBody MemberDto.JoinResultDto memberDto) {
-        return memberService.signUp(memberDto);
+    public ResponseEntity<ApiResponse<String>> signUp(@RequestParam
+                                                          @NotNull @NotBlank
+                                                          @Size(max=20)String nickname) {
+        return memberService.signUp(nickname);
     }
 
     // 로그인
     @PostMapping("/login")
-    public ResponseEntity<ApiResponse<String>> login(@RequestBody MemberDto.JoinResultDto memberDto) {
-        return memberService.login(memberDto);
+    public ResponseEntity<ApiResponse<String>> login(@RequestParam
+                                                         @NotNull @NotBlank
+                                                         @Size(max=20)String nickname) {
+        return memberService.login(nickname);
     }
 }
