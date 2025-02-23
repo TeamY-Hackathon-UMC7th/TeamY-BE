b934b750b7aab74ecbc11efc734fc6cad1e0d8c4
OneE <bestoneeee@naver.com>
2025-02-23 20:33:42 +0900

    feat: 이메일 검증 api, 닉네임 변경 api  (#41)
    * Update README.md
    
    * feat: 이메일 검증 api, 닉네임 변경 api (#40)
    
    * chore: api명세에 맞게 경로 수정
    
    * chore: 에러처리 수정
    
    * feat: 이메일 검증 api, 닉네임 변경 api
    
    ---------
    
    Co-authored-by: JIMIN KIM <102471138+jimin-fundamental@users.noreply.github.com>

--- src/main/java/hackathon/spring/config/SecurityConfig.java
+++ src/main/java/hackathon/spring/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers("/auth/join", "/auth/login").permitAll()
+                        .requestMatchers("/auth/join", "/auth/login", "auth/email").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
