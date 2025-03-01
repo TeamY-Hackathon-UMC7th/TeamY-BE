2b34f716c0dad07fe5eb8080fe256c36332e723a
Karen <mingki080808@gmail.com>
2025-03-01 16:18:03 +0900

    [fix] auth를 /로 바꿈

--- src/main/java/hackathon/spring/web/controller/MemberRestController.java
+++ src/main/java/hackathon/spring/web/controller/MemberRestController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class MemberRestController {
     private final MemberService memberService;
